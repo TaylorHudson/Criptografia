c7d646e55cdaa879403f96b220459d3b355c7cb3
Taylor Hudson <taylorhudsonsantos@gmail.com>
2022-09-21 13:13:02 -0300

    adiciona interação com o usuário

--- criptografia.py
+++ criptografia.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-chave = np.array([2,1,-1,4])
-chave = np.resize(chave,(2,2))
-chave_inversa = np.linalg.inv(chave)
-
 def cifrar(palavra, chave):
     if (len(palavra) % 2 != 0):
         palavra += ' '
@@ -23,7 +19,6 @@ def cifrar(palavra, chave):
         linha = np.dot(chave, matriz[i])
         matriz_cifrada.append(linha) 
     matriz_cifrada = np.array(matriz_cifrada)
-
     return matriz_cifrada
 
 def decifrar(matriz_cifrada, chave_inversa):
@@ -33,10 +28,9 @@ def decifrar(matriz_cifrada, chave_inversa):
         linha = np.dot(chave_inversa,matriz_cifrada[i])
         matriz_decifrada.append(linha)
     matriz_decifrada = np.array(matriz_decifrada)
-
     return matriz_decifrada
 
-def retornar_mensagem(matriz_decifrada):
+def retornar_mensagem_decifrada(matriz_decifrada):
     msg_decifrada = ''
     for i in range(len(matriz_decifrada)):
         for j in range(len(matriz_decifrada[i])):
@@ -44,5 +38,14 @@ def retornar_mensagem(matriz_decifrada):
                 msg_decifrada += chr(int(matriz_decifrada[i][j]) + 1)
             else:
                 msg_decifrada += chr(int(matriz_decifrada[i][j]))
-    
     return msg_decifrada
+
+def retornar_mensagem_cifrada(matriz_cifrada):
+    msg = ''
+    for i in range(len(matriz_cifrada)):
+        for j in range(len(matriz_cifrada[i])):
+            if (matriz_cifrada[i][j] != matriz_cifrada[-1][-1]):
+                msg += str(matriz_cifrada[i][j]) + ', '
+            else:
+                msg += str(matriz_cifrada[i][j])
+    return msg
--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 from criptografia import *
+import os
 
-mensagem_para_cifrar = input('Digite uma frase para ser cifrada:')
+def formatar_entrada(numeros):
+    lista_numeros = numeros.split(", ")
+    matriz_criptografada = []
+    for i in range(len(lista_numeros)):
+        if (i % 2 == 0):
+            linha = []
+            linha.append(int( lista_numeros[i] ))
+            linha.append(int( lista_numeros[i + 1] ))
+            matriz_criptografada.append(linha)
+    matriz_criptografada = np.array(matriz_criptografada)
+    return matriz_criptografada
 
-matriz_cifrada = cifrar(mensagem_para_cifrar, chave)
-matriz_decifrada = decifrar(matriz_cifrada, chave_inversa)
-msg = retornar_mensagem(matriz_decifrada)
+def principal(chave, chave_inversa):
+    try:
+        opc = 0
+        while (opc != 2):
+            opc = int(input('O que deseja fazer? Criptografar(0), descriptografar(1) ou parar o programa(2)? '))
 
-print('Matriz cifrada: ')
-print(matriz_cifrada)
-print('Matriz decifrada:')
-print(matriz_decifrada)
-print(f'\nA mensagem que você digitou foi: {msg}')
+            if (opc == 0):
+                os.system('cls')
+                mensagem_para_cifrar = input('Digite uma mensagem para ser cifrada: ')
+
+                matriz_cifrada = cifrar(mensagem_para_cifrar, chave)
+                print(f'Essa é sua matriz cifrada: \n{matriz_cifrada}')
+
+                mensagem = retornar_mensagem_cifrada(matriz_cifrada)
+                print(f'E essa é sua mensagem cifrada: {mensagem}')
+            elif (opc == 1):
+                os.system('cls')
+                numeros = input('Digite a mensagem cifrada: ')
+                matriz_criptografada = formatar_entrada(numeros)
+
+                matriz_decifrada = decifrar(matriz_criptografada, chave_inversa)
+                print(f'Essa é sua matriz decifrada: \n{matriz_decifrada}')
+
+                mensagem = retornar_mensagem_decifrada(matriz_decifrada)
+                print(f'E essa é sua mensagem decifrada: {mensagem}')
+            elif (opc != 2):
+                os.system('cls')
+                print('Digite uma entrada válida.')
+    except:
+        os.system('cls')
+        print('Algo deu errado,tente novamente.')
+        principal(chave, chave_inversa)
+
+chave = np.array([[2,1],[-1,4]])
+chave_inversa = np.linalg.inv(chave)
+
+principal(chave, chave_inversa)
